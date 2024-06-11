85a9e3d01f34cb5233422d242e2082385e708495
Douglas Malan <125991889+MilanDouglas@users.noreply.github.com>
2024-06-11 11:17:17 +0000

    Looping over Lists
    Practicing with List comprehension and enumeration.

--- main.py
+++ main.py
@@ -1,1 +1,3 @@
-namelist = ["Tom", "Nancy", "Drew", "Steph"]
+name_list = ["Thayla", "James", "Maria", "Kuni"]
+new_name_comprehension = [name+" Malan" for name in name_list]
+print(new_name_comprehension)
