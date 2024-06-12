88c619634983e9108d74aace3036630bf43b3c57
Douglas Malan <125991889+MilanDouglas@users.noreply.github.com>
2024-06-12 07:21:14 +0000

    Conditionals Practice in Games
    Using conditionals to make a small wizard game in Python (level 2).

--- main.py
+++ main.py
@@ -1,3 +1,21 @@
-names = ["Thuckzin", "Kabza", "Stinja", "Maphorisa"]
-if "Stinja" in names:
-  print(1)
+#constants for behaviour messages
+FRIENDLY_MESSAGE = "The door opens!"
+HOSTILE_MESSAGE = "The door stays closed."
+
+#Get the creatures behaviour from the player
+def get_creature_behaviour():
+  return input("What is the behaviour: friendly or hostile? ").strip().lower()
+
+#Use if-else statement to determine the action based on the creatures behaviour
+def determine_action(creature_behaviour):
+  if creature_behaviour == "friendly":
+    print(FRIENDLY_MESSAGE)
+  else:
+    print(HOSTILE_MESSAGE)
+
+def main():
+  behaviour = get_creature_behaviour()
+  determine_action(behaviour)
+
+if __name__ == "__main__":
+  main()
