5d3b232f66b6bd05865b5b232530406bee438d92
Douglas Malan <125991889+MilanDouglas@users.noreply.github.com>
2024-06-12 08:21:15 +0000

    elif - Conditionals
    Using elif statements to determine different outcomes.

--- main.py
+++ main.py
@@ -3,19 +3,22 @@ FRIENDLY_MESSAGE = "The door opens!"
 HOSTILE_MESSAGE = "The door stays closed."
 
 #Get the creatures behaviour from the player
-def get_creature_behaviour():
-  return input("What is the behaviour: friendly or hostile? ").strip().lower()
+def get_time_of_day():
+  return input("What time of the day is it? ").strip().lower()
 
 #Use if-else statement to determine the action based on the creatures behaviour
-def determine_action(creature_behaviour):
-  if creature_behaviour == "friendly":
-    print(FRIENDLY_MESSAGE)
+def determine_action(time_of_day):
+  if time_of_day == "morning" or time_of_day == "afternoon":
+    return (FRIENDLY_MESSAGE)
+  elif time_of_day == "evening" or time_of_day == "night":
+    return (HOSTILE_MESSAGE)
   else:
-    print(HOSTILE_MESSAGE)
-
+    return (HOSTILE_MESSAGE)
+  
 def main():
-  behaviour = get_creature_behaviour()
-  determine_action(behaviour)
+  time_of_day = get_time_of_day()
+  action = determine_action(time_of_day)
+  print(action)
 
 if __name__ == "__main__":
   main()
