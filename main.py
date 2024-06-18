a31972eeeca27a36ce939f375282ec7d1dee7acd
Douglas Malan <125991889+MilanDouglas@users.noreply.github.com>
2024-06-18 12:11:31 +0000

    Keyword Arguments
    Keyword Arbitrary Arguments.(*names)

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-#Functions with Multiple Arguments
-def my_function3(number1, number2):
-  print ("Number 1 is ", number1)
-  print ("Number 2 is ", number2)
+#Keyword Argument
+def print_names(*names):
+  for name in names:
+    print(name)
 
-my_function3(number2=3, number1=7)
+print_names("Will", "Jay", "Kay", "May")
