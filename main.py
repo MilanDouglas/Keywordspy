a16f586254a044780b4f32e16c6bcda55bf97874
Douglas Malan <125991889+MilanDouglas@users.noreply.github.com>
2024-06-19 11:01:37 +0000

    Classes  with (vars)
    Here we can get the data from this class object by using the vars() command.

--- main.py
+++ main.py
@@ -5,9 +5,5 @@ class Emperor:
     self.coronation = coronation
     self.death = death
 
-  def __str__(self):
-    return (f"Emperor {self.name}, born in {self.birth},"
-            f"was coronated in {self.coronation}, died in {self.death}")
-
 charlemagne = Emperor("Charlemagne", 742, 800, 814)
-print(charlemagne)
+print(vars(charlemagne))
