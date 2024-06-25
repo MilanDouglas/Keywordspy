0e2097307a8d44fbab04faf216fdccd4cf6216d2
Douglas Malan <125991889+MilanDouglas@users.noreply.github.com>
2024-06-25 11:17:24 +0000

    Import a csv dictionary file from Excel into python.
    using filereader function of csv to read a file.

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
+import csv
+
 books =[]
 
-for i in range(3):
-  book = dict()
-  book["Title"] = input("Enter book title: ")
-  book["Author"] = input("Enter author name: ")
-  books.append(book)
-
-for book in books:
-  print(book)
+with open()
