33e0a7c5a5adc9507b992f53e841f9db84e0312f
Douglas Malan <125991889+MilanDouglas@users.noreply.github.com>
2024-06-27 07:48:32 +0000

    Working with Files
    How to Write Dta to a Text File.

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import requests
+new_string = "This is a new string"
+with open ('../data/sample2.txt', "w") as f:
+  f.write(new_string)
 
-with open ('../data/sample.txt', "r") as f:
-  data = f.read()
 
-print(data.splitlines())
