83ecc7712e633cf533b1d9a1b0e883ede73d8c0e
Douglas Malan <125991889+MilanDouglas@users.noreply.github.com>
2024-08-06 16:16:58 +0000

    Objects and Classes Practice
    from main import main
    
    e1 = ouremployees("Bob", "Sales", "Director od Sales", 100000, 20)
    e2 = ouremployees("Linda", "Executive", "CIO", 150000, 16)
    
    print(e1.name)
    print(e2.role)
    print(e1.eligible_for_retirement())

--- main.py
+++ main.py
@@ -12,3 +12,13 @@ class Employees:
       return True
     else:
       return False
+
+#object and code to in another python file
+from main import main
+
+e1 = ouremployees("Bob", "Sales", "Director od Sales", 100000, 20)
+e2 = ouremployees("Linda", "Executive", "CIO", 150000, 16)
+
+print(e1.name)
+print(e2.role)
+print(e1.eligible_for_retirement())
