e0b8b8dfea84a73857580dd029bbb6ea38a8197e
Douglas Malan <125991889+MilanDouglas@users.noreply.github.com>
2024-08-06 15:14:03 +0000

    Project02 - Mini Caclculator
    A mini calculator to do small calculations like Addition, Subtraction, Division, Multiplication and Exponents.

--- main.py
+++ main.py
@@ -1,40 +1,17 @@
-from os import error
-import socket
-import sys
-from datetime import datetime
+#Mini Caclculator
+x = float(input("Give me a number: "))
+o = input("Give an operator: ")
+y = float(input("Give me another number: "))
 
-#Define our Target
-if len(sys.argv) == 2:
-  target = socket.gethostbyname(sys.argv[1]) #Translate hostname to IPv4
+if o == "+":
+  print(x + y)
+elif o == "-":
+    print(x - y)
+elif o == "/":
+  print(x / y)
+elif o == "*":
+  print(x * y)
+elif o == "**" or o == "^":
+  print(x ** y)
 else:
-  print("Invalid amount of arguments.")
-  print("Syntax: python3 scanner.py <ip>")
-
-#Add a pretty banner
-print("." * 50)
-print("Scanning target: "+target)
-print("Time started: "+str(datetime.now()))
-print("." * 50)
-
-try: 
-  for port in range(50,85):
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    socket.setdefaulttimeout(1)
-    result = s.connect_ex((target,port))
-    if result == 0:
-      print(f"Port {port} is open")
-      s.close()
-
-except KeyboardInterrupt:
-  print("\n Exiting program.")
-  sys.exit()
-
-except socket.gaierror:
-  print("Hostname could not be resolved.")
-  sys.exit()
-
-except socket.error:
-  print("Could not connect to the server.")
-  sys.exit()
-  
-  
+  print("Unknown Operator.")
